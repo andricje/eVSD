4c1d3f9bc3353ad70fc6abf82c583600aa9e28bb
Veliki Humus <humuseater@proton.me>
2025-05-20 20:37:20 +0000

    Inherit GovernorProposalGuardian in the EvsdGovernor contract to enable canceling a proposal at any stage Set a lower number of optimizer runs to reduce the compiled contract size

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,12 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",  
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200  
+      }
+    }
+  },
   networks: {
     hardhat: {
       accounts: {
         count: 50,
       },
+
     },
   },
 };
