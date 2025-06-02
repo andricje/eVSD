8e65fa9a9fcd771a041c47bbf3e84c84ac565043
Veliki Humus <humuseater@proton.me>
2025-06-02 18:38:25 +0000

    Revert "Add two more test accounts to the address name map"
    This reverts commit 9a177a09a75475f231f76b84701da48851df67b9.

--- constants/address-name-map.ts
+++ constants/address-name-map.ts
@@ -1,6 +1,4 @@
 export const addressNameMap: Record<string, string> = {
   "0x5DE5187A457FA721Df7F2dd0e673B9d0a2500788": "Veliki Humus",
   "0xb2C3Df6d6467f71ACc09b8Dc40D487e084407873": "Mali Humus",
-  "0x401DdEd54d7df1e5b0ef8AA4C5fBDF1F9e83502d": "Filozofski test nalog",
-  "0x4a4A2393809f21310183bCe4609D6B6abdEb8B02": "Višer test nalog",
 };
