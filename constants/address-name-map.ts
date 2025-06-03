91287d68e6f1777917172e6b3d19d2f8267eee1d
Veliki Humus <humuseater@proton.me>
2025-06-03 23:27:22 +0000

    Add a new account to the addressNameMap

--- constants/address-name-map.ts
+++ constants/address-name-map.ts
@@ -3,4 +3,5 @@ export const addressNameMap: Record<string, string> = {
   "0xb2C3Df6d6467f71ACc09b8Dc40D487e084407873": "Mali Humus",
   "0x401DdEd54d7df1e5b0ef8AA4C5fBDF1F9e83502d": "Filozofski test nalog",
   "0x4a4A2393809f21310183bCe4609D6B6abdEb8B02": "Višer test nalog",
+  "0x696a45A7150c1d294Ce6E4168A7373b6c38aBC40": "FON test nalog",
 };
