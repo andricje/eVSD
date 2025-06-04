ec2ba5e315d6a1cb90317fcd310089e713a8b4c4
Veliki Humus <humuseater@proton.me>
2025-06-04 23:32:28 +0000

    Set arbitrum sepolia addresses

--- contracts/EvsdToken.sol
+++ contracts/EvsdToken.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.27;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EvsdToken is ERC20, ERC20Permit, ERC20Votes {
-    constructor(address recipient)
+contract EvsdToken is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
+    constructor(address recipient, address initialOwner)
         ERC20("EvsdToken", "EVSD")
         ERC20Permit("EvsdToken")
+        Ownable(initialOwner)
     {
         _mint(recipient, 100000 * 10 ** decimals());
     }
