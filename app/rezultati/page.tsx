a946fd4676691dbe01261cf2bb15df1c4d61d47b
rajca21 <2nikolar1@gmail.com>
2025-06-14 22:44:47 +0200

    Filtering results by users that voted

--- app/rezultati/page.tsx
+++ app/rezultati/page.tsx
@@ -43,6 +43,7 @@ import { useWallet } from "@/context/wallet-context";
 import { useRouter } from "next/navigation";
 import { Proposal, User } from "@/types/proposal";
 import { addressNameMap } from "@/constants/address-name-map";
+import { Badge } from "@/components/ui/badge";
 
 function FilterResults({
   filteredProposals,
@@ -109,18 +110,28 @@ function FilterResults({
 }
 
 // ComboBox za pretragu korisnika
-function AuthorsCombobox({
-  onSelect,
+function UsersCombobox({
+  onSelectAuthor,
+  onAddUserToFollow,
+  isAuthorComboBox = false,
 }: {
-  onSelect?: React.Dispatch<React.SetStateAction<User | null>>;
+  onSelectAuthor?: React.Dispatch<React.SetStateAction<User | null>>;
+  onAddUserToFollow?: (user: User) => void;
+  isAuthorComboBox?: boolean;
 }) {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState<User | null>(null);
 
-  const handleSelect = (user: User) => {
-    setSelected(user);
+  const handleSelect = (user: User | null) => {
+    if (isAuthorComboBox) {
+      setSelected(user);
+      onSelectAuthor?.(user);
+    } else {
+      if (user) {
+        onAddUserToFollow?.(user);
+      }
+    }
     setOpen(false);
-    onSelect?.(user);
   };
 
   return (
@@ -129,14 +140,18 @@ function AuthorsCombobox({
         <Button
           variant="outline"
           role="combobox"
-          className="w-full sm:w-[180px]"
+          className={`w-full ${isAuthorComboBox ? "sm:w-[180px]" : "sm:w-[260px]"}`}
         >
           <div className="flex w-full items-center gap-2 -ml-2">
             <UserIcon className="h-4 w-4" />
-            {selected ? (
-              <span className="flex items-center gap-2">{selected.name}</span>
+            {isAuthorComboBox ? (
+              selected ? (
+                <span className="flex items-center gap-2">{selected.name}</span>
+              ) : (
+                "Aутор"
+              )
             ) : (
-              "Изаберите аутора"
+              "Прикажи гласове корисника"
             )}
           </div>
         </Button>
@@ -146,11 +161,21 @@ function AuthorsCombobox({
           <CommandInput placeholder="Претражите кориснике..." />
           <CommandList>
             <CommandEmpty>Нема резултата.</CommandEmpty>
+            {isAuthorComboBox && (
+              <CommandItem
+                value={undefined}
+                onSelect={() => handleSelect(null)}
+                className="cursor-pointer"
+              >
+                <span className="font-semibold">Сви</span>
+              </CommandItem>
+            )}
             {usersFromAddressNameMapRecord(addressNameMap).map((user) => (
               <CommandItem
                 key={user.address}
                 value={user.name}
                 onSelect={() => handleSelect(user)}
+                className="cursor-pointer"
               >
                 <span className="font-semibold">{user.name}</span>
                 {clipAddress(user.address)}
@@ -173,6 +198,13 @@ export default function RezultatiPage() {
   const [searchTerm, setSearchTerm] = useState("");
   const [filterDate, setFilterDate] = useState("all");
   const [filterAuthor, setFilterAuthor] = useState<User | null>(null);
+  const [usersToFollow, setUsersToFollow] = useState<User[]>([]);
+
+  const handleAddUserToFollow = (user: User) => {
+    if (!usersToFollow.some((u) => u.address === user.address)) {
+      setUsersToFollow((prev) => [...prev, user]);
+    }
+  };
 
   // Sortiranje predloga hronološki - najnoviji na vrhu
   const sortedProposals = [...proposals].sort((a, b) => {
@@ -210,7 +242,19 @@ export default function RezultatiPage() {
       matchesAuthor = proposal.author.address === filterAuthor.address;
     }
 
-    return matchesSearch && matchesDate && matchesAuthor;
+    let matchesUsersToFollow = true;
+
+    if (usersToFollow?.length > 0) {
+      matchesUsersToFollow = usersToFollow.some((user) =>
+        proposal.voteItems.some((voteItem) =>
+          voteItem.userVotes.has(user.address)
+        )
+      );
+    }
+
+    return (
+      matchesSearch && matchesDate && matchesAuthor && matchesUsersToFollow
+    );
   });
 
   return (
@@ -218,36 +262,76 @@ export default function RezultatiPage() {
       <Header />
       <main className="flex-1 w-full max-w-full py-6 px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col gap-6">
-          <div className="flex flex-col lg:flex-row items-start lg:items-center lg:justify-between gap-4">
-            <h1 className="text-3xl w-full font-bold flex-shrink-0 md:max-w-[50%]">
+          <div className="flex flex-col lg:flex-row lg:justify-between gap-4">
+            <h1 className="text-3xl font-bold flex-shrink-0">
               Објављени резултати гласања
             </h1>
-            <div className="flex flex-col sm:flex-row sm:items-center gap-2 min-w-[300px]">
-              <div className="relative min-w-[200px]">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                  type="search"
-                  placeholder="Претрага предлога..."
-                  className="pl-8 w-full md:w-[260px]"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                />
-              </div>
-              <Select value={filterDate} onValueChange={setFilterDate}>
-                <SelectTrigger className="w-full md:w-[180px] min-w-[150px]">
-                  <div className="flex items-center gap-2">
-                    <Calendar className="h-4 w-4" />
-                    <SelectValue placeholder="Филтер по датуму" />
+            <div className="flex flex-col gap-2 w-full sm:w-[450px] 2xl:w-auto">
+              <div className="flex flex-col 2xl:flex-row gap-2">
+                <div className="flex flex-col sm:flex-row sm:items-center gap-2">
+                  <div className="relative w-full sm:w-[260px]">
+                    <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                    <Input
+                      type="search"
+                      placeholder="Претрага предлога..."
+                      className="pl-8 w-full sm:w-[260px]"
+                      value={searchTerm}
+                      onChange={(e) => setSearchTerm(e.target.value)}
+                    />
                   </div>
-                </SelectTrigger>
-                <SelectContent className="w-full min-w-[150px]">
-                  <SelectItem value="all">Све време</SelectItem>
-                  <SelectItem value="month">Последњи месец</SelectItem>
-                  <SelectItem value="quarter">Последња 3 месеца</SelectItem>
-                  <SelectItem value="year">Последња година</SelectItem>
-                </SelectContent>
-              </Select>
-              <AuthorsCombobox onSelect={setFilterAuthor} />
+                  <Select value={filterDate} onValueChange={setFilterDate}>
+                    <SelectTrigger className="w-full md:w-[180px] min-w-[150px] sm:max-w-[300px]">
+                      <div className="flex items-center gap-2">
+                        <Calendar className="h-4 w-4" />
+                        <SelectValue placeholder="Филтер по датуму" />
+                      </div>
+                    </SelectTrigger>
+                    <SelectContent className="w-full min-w-[150px]">
+                      <SelectItem value="all">Све време</SelectItem>
+                      <SelectItem value="month">Последњи месец</SelectItem>
+                      <SelectItem value="quarter">Последња 3 месеца</SelectItem>
+                      <SelectItem value="year">Последња година</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="flex flex-col sm:flex-row sm:items-center gap-2 w-full 2xl:w-[448px]">
+                  <div className="order-2 sm:order-1 2xl:order-2">
+                    <UsersCombobox
+                      isAuthorComboBox={false}
+                      onAddUserToFollow={handleAddUserToFollow}
+                    />
+                  </div>
+                  <div className="order-1 sm:order-2 2xl:order-1">
+                    <UsersCombobox
+                      onSelectAuthor={setFilterAuthor}
+                      isAuthorComboBox={true}
+                    />
+                  </div>
+                </div>
+              </div>
+              {usersToFollow?.length > 0 && (
+                <div className="flex flex-col items-start lg:items-end gap-1 max-w-[440px] xl:max-w-full">
+                  <h4 className="text-sm text-gray-500">
+                    Корисници чије гласове пратите
+                  </h4>
+                  <div className="flex flex-row flex-wrap lg:justify-end w-full gap-2 max-w-[440px] xl:max-w-full">
+                    {usersToFollow.map((user) => (
+                      <Badge
+                        variant="outline"
+                        key={user.address}
+                        onClick={() =>
+                          setUsersToFollow((prev) =>
+                            prev.filter((u) => u.address !== user.address)
+                          )
+                        }
+                        className="cursor-pointer"
+                      >
+                        {user.name}
+                      </Badge>
+                    ))}
+                  </div>
+                </div>
+              )}
             </div>
           </div>
 
